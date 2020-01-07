fc342523c020abbe4e6618d63a67d10e5f3419df
epons <ticquique@gmail.com>
2020-01-07 20:37:38 +0100

    A

--- backend/src/interfaces/reportero.ts
+++ backend/src/interfaces/reportero.ts
@@ -2,9 +2,11 @@ import { Categoria } from "./categoria";
 
 export interface Reportero {
   _id: any;
+  dni: string;
   name: string;
+  apellidos: string;
+  direccion: string;
+  ciudad: string;
+  cp: string;
   categoria: Categoria;
-  descripcion: string;
-  equipoFotografico: string;
-  resumenCV: string;
 }
